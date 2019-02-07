228482e4d61a056be517628332759046fc259b28
Puroguramu <peterh320265@gmail.com>
2019-02-07 17:32:06 +0800

    以解決store get資料問題 但是並未模組化

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -20,13 +20,13 @@ export default new Vuex.Store({
     mutations: {
         getcontents(state, data) {
             // console.log(data[0].title)
-            let i
-            for (i = 0; i < data.length; i++) {
-                data[i].footer = moment(
-                    data[i].createdatetime,
-                    'YYYYMMDD h:mm:ss a'
-                ).fromNow()
-            }
+            // let i
+            // for (i = 0; i < data.length; i++) {
+            //     data[i].footer = moment(
+            //         data[i].createdatetime,
+            //         'YYYYMMDD h:mm:ss a'
+            //     ).fromNow()
+            // }
             state.contents = data.reverse()
         },
         setcontents(state, data) {
