8878544b200052a15456acc27d97f5f5ad1beeb2
newgithubuse <peterh320267@gmail.com>
2019-03-03 14:10:43 +0800

    新增login畫面

---
+++ resources/js/components/Loginsample.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loginpage">
+    <p>
+      使用者登入
+    </p>
+    <input
+      type="text"
+      placeholder="請輸入帳號"
+      v-model.trim="account"
+    ><br>
+    <input
+      type="text"
+      placeholder="請輸入密碼"
+      v-model.trim="password"
+    ><br>
+    <button @click='login'>
+      登入
+    </button>
+  </div>
+</template>
+<style scope>
+  html {
+    height: 100%;
+  }
+  body {
+    min-height: 100%;
+  }
+  .loginpage {
+    width: 1000px;
+    background-color: lavender;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+<script>
+  import store from "@/store";
+  import { router } from "@/main.js";
+  export default {
+    data() {
+      return {
+        account: "",
+        password: ""
+      };
+    },
+    methods: {
+      login() {
+        this.$store.dispatch("Login", true);
+        window.localStorage.setItem("loginstatus", true);
+        this.$router.push("/");
+      }
+    }
+  };
+</script>
