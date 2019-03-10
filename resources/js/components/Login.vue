bf0546a853a17b772df5386b6578dc3143868a36
Puroguramu <peterh320265@gmail.com>
2019-03-10 18:44:37 +0800

    修改login頁面配色 還須調整

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,16 +1,33 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>校園工具人派遣</el-header>
     <el-main>
-      <div class="logintitle">
-        <div>登入</div>
-        <div>帳號</div>
-        <div><input type="text"></div>
-        <div>密碼</div>
-        <div><input type="text"></div>
+      <div class="logintitle">coming soon. . .</div>
+      <div class="login">
+        <div class="logincontent">帳號</div>
+        <div class="logincontent">
+          <el-input
+            v-model="account"
+            placeholder="請輸入帳號"
+          ></el-input>
+        </div>
+        <div class="logincontent">密碼</div>
+        <div class="logincontent">
+          <el-input
+            placeholder="請輸入密碼"
+            v-model="password"
+            show-password
+          ></el-input>
+        </div>
+        <div class="logincontent">
+          <el-button
+            type="primary"
+            @click.native="login"
+          >登入</el-button>
+        </div>
       </div>
     </el-main>
-    <el-footer>Footer</el-footer>
+    <el-footer>Copyright &copy; 2019 Apple Inc.</el-footer>
   </el-container>
 </template>
 <style scope>
@@ -26,25 +43,45 @@
   .el-container {
     height: 100%;
   }
+  .el-header {
+    font-size: 2em;
+  }
   .el-header,
   .el-footer {
-    background-color: #b3c0d1;
+    background-color: rgba(0, 27, 128, 0.5);
     color: #333;
     text-align: center;
     line-height: 60px;
   }
   .el-main {
-    background-color: #e9eef3;
+    position: relative;
+    background-color: #eae2d6;
     color: #333;
-    display: flex !important;
-    justify-content: center;
-    align-items: center;
   }
   .logintitle {
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    text-align: center;
+  }
+  .login {
+    z-index: 1;
+    width: 40%;
+    height: 60%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+  }
+  .logincontent + .logincontent {
+    margin-top: 10px;
   }
 </style>
 <script>
